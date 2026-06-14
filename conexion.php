51076200e12821869cbf5907d27afb13b9bea370
Josue Daniel Hernandez Giron <72953337+josueherz@users.noreply.github.com>
2026-06-14 00:46:37 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $usuario = 'root';
 $contrasena = '';
 $basedatos = 'prueba';
-echo ="Esto solo es una prueba"
+echo ="ESTAS PRUEBAS SON EXTRAÑAS"
 $conexion = new mysqli($host, $usuario, $contrasena, $basedatos);
 
 if ($conexion->connect_error) {
